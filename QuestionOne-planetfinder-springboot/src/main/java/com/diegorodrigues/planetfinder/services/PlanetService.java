1c73d01c3dd50a939400c46beb7f7a2ea3cc50b1
Diego Rodrigues <diego.crodrigues8@outlook.com>
2023-06-19 19:55:08 -0300

    Exception handling - delete

--- QuestionOne-planetfinder-springboot/src/main/java/com/diegorodrigues/planetfinder/services/PlanetService.java
+++ QuestionOne-planetfinder-springboot/src/main/java/com/diegorodrigues/planetfinder/services/PlanetService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.diegorodrigues.planetfinder.entities.Planet;
@@ -12,29 +13,33 @@ import com.diegorodrigues.planetfinder.services.exceptions.ResourceNotFoundExcep
 
 @Service
 public class PlanetService {
-	
+
 	@Autowired
 	private PlanetRepository repository;
-	
+
 	public Planet insert(Planet obj) {
 		return repository.save(obj);
 	}
-	
-	public List<Planet> findAll(){
+
+	public List<Planet> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Planet findById(Long id) {
 		Optional<Planet> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public List<Planet> findByName(String name) {
 		List<Planet> list = repository.findByName(name);
 		return list;
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 }

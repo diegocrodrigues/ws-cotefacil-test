f19591fdd5ce975ed239881d7bc9083eed035754
Diego Rodrigues <diego.crodrigues8@outlook.com>
2023-06-19 14:47:12 -0300

    JPA repository, dependency injection, database seeding

---
+++ QuestionOne-planetfinder-springboot/src/main/java/com/diegorodrigues/planetfinder/config/TestConfig.java
@@ -0,0 +1,31 @@
+package com.diegorodrigues.planetfinder.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.diegorodrigues.planetfinder.entities.Planet;
+import com.diegorodrigues.planetfinder.repositories.PlanetRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	
+	@Autowired
+	private PlanetRepository planetRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Planet p1 = new Planet(null, "Diegolandia", "frio", "arenoso");
+		Planet p2 = new Planet(null, "Planetinha do DG", "quente", "umido");
+		
+		planetRepository.saveAll(Arrays.asList(p1, p2));
+		
+	} 
+	
+	
+}
---
+++ QuestionOne-planetfinder-springboot/src/main/java/com/diegorodrigues/planetfinder/repositories/PlanetRepository.java
@@ -0,0 +1,9 @@
+package com.diegorodrigues.planetfinder.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegorodrigues.planetfinder.entities.Planet;
+
+public interface PlanetRepository extends JpaRepository<Planet, Long>{
+	
+}

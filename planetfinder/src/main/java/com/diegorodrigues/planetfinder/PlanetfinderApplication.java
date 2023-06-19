fa1b5c4c5df75247d820bb13e798053a9a34344d
Diego Rodrigues <diego.crodrigues8@outlook.com>
2023-06-19 13:42:14 -0300

    Project created

---
+++ planetfinder/src/main/java/com/diegorodrigues/planetfinder/PlanetfinderApplication.java
@@ -0,0 +1,13 @@
+package com.diegorodrigues.planetfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetfinderApplication.class, args);
+	}
+
+}
---
+++ planetfinder/src/test/java/com/diegorodrigues/planetfinder/PlanetfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegorodrigues.planetfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
